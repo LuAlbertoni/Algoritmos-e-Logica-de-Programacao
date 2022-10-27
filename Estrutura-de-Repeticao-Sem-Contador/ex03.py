78f54ab4e1f3dc7cb067daa58b838e33adee9688
Luís Felipe Albertoni <lualbertoni@protonmail.com>
2022-10-27 08:46:33 -0300

    Corrigindo exercício

--- Estrutura-de-Repeticao-Sem-Contador/ex03.py
+++ Estrutura-de-Repeticao-Sem-Contador/ex03.py
@@ -1,17 +1,26 @@
 # Faça um programa que receba a altura de várias pessoas. Encontre e apresente a altura da pessoa mais alta e da mais baixa e seus respectivos nomes. Para encerrar a entrada de dados, zero na altura, mas esta não poderá ser considerada como resposta da altura da pessoa mais baixa.
 
-altura = float(input('Insira a altura ou "0" para finalizar: '))
+altura = float(input('Insira a altura: '))
+nome = input('Insira o nome da pessoa: ')
 
 maior = altura
 menor = altura
+nomeMaior = nome
+nomeMenor = nome
 
 while altura != 0:
   if altura != 0:
     if altura > maior:
+      nomeMaior = nome
       maior = altura
     elif altura < menor:
+      nomeMenor = nome
       menor = altura
       
   altura = float(input('Insira a altura ou "0" para finalizar: '))
 
-print(f'Pessoa mais baixa: {menor:.2f}, pessoa mais alta: {maior:.2f}')
+  if altura != 0:
+    nome = input('Insira o nome da pessoa: ')
+
+
+print(f'Pessoa mais baixa: {nomeMenor} - {menor:.2f}, pessoa mais alta: {nomeMaior} - {maior:.2f}')
