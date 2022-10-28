5d5b0d5723a1dbee15c0a20912985fb46bd24d14
Luís Felipe Albertoni <lualbertoni@protonmail.com>
2022-10-28 12:29:05 -0300

    Corrigido erro de salvar número negativo na variável "menor"

--- Estrutura-de-Repeticao-Sem-Contador/ex06.py
+++ Estrutura-de-Repeticao-Sem-Contador/ex06.py
@@ -3,14 +3,18 @@
 #   Para valores negativos, deve ser enviada uma mensagem;
 #   Esses valores (zero e negativos) não entrarão na lógica de encontrar o maior e o menor valor.
 
-num = int(input('Insira um número positivo inteiro ou "0" para finalizar: '))
-maior = num
-menor = num
+maior = 0
+menor = 0
+
+num = int(input('Insira um número positivo inteiro: '))
 
 while num != 0:
     if num < 0:
         print('Números negativos não são permitidos!')
     else:
+        if menor == 0:
+            menor = num
+            
         if num > maior:
             maior = num
         elif num < menor:
